c86d081d23f7658b46a87d146b88f20f08c7ab76
Lovelybit <dmitrimargolin@gmail.com>
2020-08-10 00:13:22 +0300

    Сделана возможность автоматической настройки передвижения юнитов в заданную сторону. Сделано (неэффективно через апдейт) подсветка базы при ее выборе.

---
+++ Assets/Scripts/UI/BaseChooseSlider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseChooseSlider : MonoBehaviour
+{
+    [SerializeField] private Slider _myChooseSlider;
+    [SerializeField] private Base _myBase;
+    [SerializeField] private GameObject _selectionColor;
+
+    /* private void Start() {
+        _myChooseSlider = GetComponentInChildren<Slider>();
+        print(_myChooseSlider);
+
+        _myBase = GetComponent<Base>();
+    } */
+
+    private void Update() {
+        if (_myBase.Owner != Owner.Player) return;
+        if (BaseSelection.BaseSelected == this.gameObject)
+        {
+            _selectionColor.SetActive(true);
+        }
+        else
+        {
+            _selectionColor.SetActive(false);
+        }
+    }
+    
+    public void OnSliderValueChange()
+    {
+        print("On SliderValueCHange");
+        var choosenUnits = _myChooseSlider.value;
+        _myBase.ChoosenUnitsForAttack = Mathf.FloorToInt(choosenUnits);
+        BaseSelection.BaseSelected = this.gameObject;
+
+        //_tmPro.text = choosenUnits.ToString();
+        
+    }
+}
